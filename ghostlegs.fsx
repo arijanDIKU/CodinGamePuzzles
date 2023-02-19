2cb107b916b0bd98735bbfb3d4452d07bde0a3a7
A J <aj@debian-BULLSEYE-live-builder-AMD64>
2023-02-19 17:18:18 +0100

    done

--- ghostlegs.fsx
+++ ghostlegs.fsx
@@ -1,22 +1,36 @@
 open System
 
+
+ 
 let token = (Console.In.ReadLine()).Split [|' '|]
 let W = int(token.[0])
 let H = int(token.[1])
-let lines = [for i in 0 .. H - 1 do let line = Console.In.ReadLine() in line]
+let toplabels = Console.In.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries) |> List.ofArray
+let lines = [for i in 0..H-3 do Console.In.ReadLine()]
+let botlabels = Console.In.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries) |> List.ofArray
+let readLine (line:string) : int list = 
+    line.Split('|', StringSplitOptions.RemoveEmptyEntries)
+    |> Array.map (function | "  " -> 0 | "--" -> 1)
+    |> List.ofArray
 
+let parsedLines = List.map readLine lines 
 
-let symbols = [for i in 1..(W/3)+1 do i] // '|--'= 3 chars and +1 for the last column '|' so begin at 0
-
-
-let traverse (l:string list) (symbols: int list) (W:int) : int list =
-   let connectors = 0::[for i in 1..3..W-1 do if l.[i]="-" then 1 else 0]
-   List.mapFold (fun prev c s -> if prev ) connectors symbols  
- 
-
-let rec solution (lines:string list) (symbols:int list) : int list =
     
-    match lines with 
-    | [] -> symbols 
-    | lne::lnes ->  traverseLine lne |> solution lnes 
- 
+let solution () : unit = 
+    let initPositions = List.init (W/3+1) id in 
+    parsedLines |> List.fold (fun currentPositions (line:int list) -> 
+                                 List.map (fun p -> let last = List.length currentPositions-1 in 
+                                                                        match p with 
+                                                                        | 0 -> p+line.[0]
+                                                                        | n when n=last -> p-line.[last-1]
+                                                                        | n -> 
+                                                                            if line.[n-1]=1 then 
+                                                                                (n-1)
+                                                                            elif line.[n]=1 then 
+                                                                                (n+1)      
+                                                                            else    
+                                                                                p) currentPositions) initPositions 
+    |> List.iteri (fun i p -> printfn "%s%s" toplabels.[i] botlabels.[p]) 
+                                                
+solution ()
+
